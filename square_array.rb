1287ef81e4af656c29a827d3aa6841dd53461f9a
Christopher Osborn <z68ozz@gmail.com>
2020-03-07 01:20:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
- array = [1,2,3] 
- array.each { |i| arr << i ** 2 }
- arr
-end 
- square_array(new_numbers)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
+
+my_arr = [1,2,3]
+p square_array(my_arr) #=> [1, 4]
